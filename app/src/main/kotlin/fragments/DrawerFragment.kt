4df5b5797b44c0c7feeaa8ec165999f86bff1f4a
Michal Faber <michalfaber@gmail.com>
2015-04-13 22:19:02 +0200

    Switch to declarative drawer implementation

--- app/src/main/kotlin/fragments/DrawerFragment.kt
+++ app/src/main/kotlin/fragments/DrawerFragment.kt
@@ -7,10 +7,11 @@ import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.michalfaber.drawertemplate.R
-import com.michalfaber.drawertemplate.views.adapters.DrawerAdapter
+import com.michalfaber.drawertemplate.views.adapters.AdapterItem
+import com.michalfaber.drawertemplate.views.adapters.drawer.*
 import kotlinx.android.synthetic.fragment_drawer.menu_items
-import views.adapters.NavigationItem
 import java.util.ArrayList
 
 public class DrawerFragment : Fragment() {
@@ -31,14 +32,31 @@ public class DrawerFragment : Fragment() {
         layoutManager.setOrientation(LinearLayoutManager.VERTICAL)
         menu_items.setLayoutManager(layoutManager)
         menu_items.setHasFixedSize(true)
-        menu_items.setAdapter(DrawerAdapter(getMenu(), listener!!))
+        menu_items.setAdapter(DrawerAdapter(getDrawerItems()))
     }
 
-    public fun getMenu(): List<NavigationItem> {
-        val items = ArrayList<NavigationItem>()
-        items.add(NavigationItem("item 1", getResources().getDrawable(R.drawable.ic_menu_check)))
-        items.add(NavigationItem("item 2", getResources().getDrawable(R.drawable.ic_menu_check)))
-        items.add(NavigationItem("item 3", getResources().getDrawable(R.drawable.ic_menu_check)))
+    public fun getDrawerItems(): MutableList<AdapterItem> {
+
+        val items: ArrayList<AdapterItem> = arrayListOf(
+                DrawerItemSpinner(arrayListOf(
+                        DrawerItemSpinner.DrawerItemSpinnerSubItem(getResources().getDrawable(R.drawable.ic_menu_check), "subitem 1", { Toast.makeText(getActivity(), "Subitem 1", Toast.LENGTH_SHORT).show() }),
+                        DrawerItemSpinner.DrawerItemSpinnerSubItem(getResources().getDrawable(R.drawable.ic_menu_check), "subitem 2", { Toast.makeText(getActivity(), "Subitem 2", Toast.LENGTH_SHORT).show() }),
+                        DrawerItemSpinner.DrawerItemSpinnerSubItem(getResources().getDrawable(R.drawable.ic_menu_check), "subitem 3", { Toast.makeText(getActivity(), "Subitem 3", Toast.LENGTH_SHORT).show() }),
+                        DrawerItemSpinner.DrawerItemSpinnerSubItem(getResources().getDrawable(R.drawable.ic_menu_check), "subitem 4", { Toast.makeText(getActivity(), "Subitem 4", Toast.LENGTH_SHORT).show() }),
+                        DrawerItemSpinner.DrawerItemSpinnerSubItem(getResources().getDrawable(R.drawable.ic_menu_check), "subitem 5", { Toast.makeText(getActivity(), "Subitem 5", Toast.LENGTH_SHORT).show() })
+                ), 2),
+                DrawerItemSimple(getResources().getDrawable(R.drawable.ic_menu_check), "standalone item", { Toast.makeText(getActivity(), "standalone item", Toast.LENGTH_SHORT).show() }),
+                DrawerItemHeader("read only section"),
+                DrawerItemSpinner(arrayListOf(
+                        DrawerItemSpinner.DrawerItemSpinnerSubItem(getResources().getDrawable(R.drawable.ic_menu_check), "abcd 1", { Toast.makeText(getActivity(), "abcd 1", Toast.LENGTH_SHORT).show() }),
+                        DrawerItemSpinner.DrawerItemSpinnerSubItem(getResources().getDrawable(R.drawable.ic_menu_check), "lorem frm 2", { Toast.makeText(getActivity(), "lorem frm 2", Toast.LENGTH_SHORT).show() }),
+                        DrawerItemSpinner.DrawerItemSpinnerSubItem(getResources().getDrawable(R.drawable.ic_menu_check), "emro lore 3", { Toast.makeText(getActivity(), "emro lore 3", Toast.LENGTH_SHORT).show() }),
+                        DrawerItemSpinner.DrawerItemSpinnerSubItem(getResources().getDrawable(R.drawable.ic_menu_check), "emarcade 4", { Toast.makeText(getActivity(), "emarcade 4", Toast.LENGTH_SHORT).show() }),
+                        DrawerItemSpinner.DrawerItemSpinnerSubItem(getResources().getDrawable(R.drawable.ic_menu_check), "orelo lorem 5", { Toast.makeText(getActivity(), "orelo lorem 5", Toast.LENGTH_SHORT).show() })
+                ), 1),
+                DrawerItemSimple(getResources().getDrawable(R.drawable.ic_menu_check), "next standalone item", { Toast.makeText(getActivity(), "next standalone item", Toast.LENGTH_SHORT).show() }),
+                DrawerItemSimple(getResources().getDrawable(R.drawable.ic_menu_check), "last standalone item", { Toast.makeText(getActivity(), "last standalone item", Toast.LENGTH_SHORT).show() })
+        )
         return items
     }
 
