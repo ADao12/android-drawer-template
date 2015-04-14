4ac748a44d670a9e5933a812b815814560795312
Michal Faber <michalfaber@gmail.com>
2015-04-14 15:01:03 +0200

    Added id field

--- app/src/main/kotlin/views/adapters/AdapterItemBase.kt
+++ app/src/main/kotlin/views/adapters/AdapterItemBase.kt
@@ -4,6 +4,11 @@ import android.support.v7.widget.RecyclerView
 import kotlin.reflect.KClass
 
 abstract class AdapterItemBase<T>(val viewHolderClass: KClass<T>): AdapterItem {
+    companion object {
+        val ID_GENERATOR_MIN_VALUE: Long = 2 shl 5
+        var nextId: Long = ID_GENERATOR_MIN_VALUE;
+    }
+    override val id = ++nextId
     override val viewType: Int = viewHolderClass.hashCode()
 
     abstract fun bindViewHolder(viewHolder: T)
